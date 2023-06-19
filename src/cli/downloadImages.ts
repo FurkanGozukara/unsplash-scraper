ae0b262cbb98c7f81dcbb25058c6da4b363b96b1
riobits <23579928+riobits@users.noreply.github.com>
2023-06-20 00:30:17 +0300

    Remove incomplete files that are being downloaded while user exit the program

--- src/cli/downloadImages.ts
+++ src/cli/downloadImages.ts
@@ -10,10 +10,35 @@ interface DownloadImagesOptions {
 }
 
 const downloadPath = path.join(path.resolve('downloads'))
+const downloaded: string[] = []
+let queryPath = ''
 
 if (!fs.existsSync(downloadPath)) {
   fs.mkdirSync(downloadPath)
 }
+
+process.on('SIGINT', () => {
+  try {
+    console.log('\n')
+    console.log('Stopping the application...')
+
+    const queryFiles = fs.readdirSync(queryPath)
+    const filesToDelete = queryFiles.filter(
+      (file) => !downloaded.includes(file)
+    )
+
+    filesToDelete.forEach((file) => {
+      fs.unlinkSync(path.join(queryPath, file))
+    })
+
+    console.log('Deleted incomplete downloads')
+
+    // Exit the process
+    process.exit(0)
+  } catch (err) {
+    process.exit(0)
+  }
+})
 
 const downloadImage = async (url: string, id: string, queryPath: string) => {
   try {
@@ -23,13 +48,14 @@ const downloadImage = async (url: string, id: string, queryPath: string) => {
     }
 
     await download.image(options)
+    downloaded.push(`${id}.jpg`)
   } catch (err) {
     console.error(err)
   }
 }
 
 const downloadImages = async ({ images, query }: DownloadImagesOptions) => {
-  const queryPath = path.join(downloadPath, query)
+  queryPath = path.join(downloadPath, query)
 
   if (fs.existsSync(queryPath)) {
     console.log('Continuing download from the past session...')
